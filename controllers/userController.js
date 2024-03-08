64db14f4487cdcd693d589012242d263a5be2e6d
kimenyu <njorogekimenyu@gmail.com>
2024-03-08 22:58:58 +0300

    changing controllers

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,10 +6,10 @@ module.exports.login = async (req, res, next) => {
     const { username, password } = req.body;
     const user = await User.findOne({ username });
     if (!user)
-      return res.json({ msg: "Incorrect Username or Password", status: false });
+      return res.status(400).json({ msg: "Incorrect Username or Password", status: false });
     const isPasswordValid = await bcrypt.compare(password, user.password);
     if (!isPasswordValid)
-      return res.json({ msg: "Incorrect Username or Password", status: false });
+      return res.status(400).json({ msg: "Incorrect Username or Password", status: false });
     delete user.password;
     return res.json({ status: true, user });
   } catch (ex) {
@@ -22,10 +22,10 @@ module.exports.register = async (req, res, next) => {
     const { username, email, password } = req.body;
     const usernameCheck = await User.findOne({ username });
     if (usernameCheck)
-      return res.json({ msg: "Username already Exists", status: false });
+      return res.status(400).json({ msg: "Username already Exists", status: false });
     const emailCheck = await User.findOne({ email });
     if (emailCheck)
-      return res.json({ msg: "Email already Exists", status: false });
+      return res.status(400).json({ msg: "Email already Exists", status: false });
     const hashedPassword = await bcrypt.hash(password, 10);
     const user = await User.create({
       email,
@@ -38,6 +38,7 @@ module.exports.register = async (req, res, next) => {
     next(ex);
   }
 };
+
 
 module.exports.getAllUsers = async (req, res, next) => {
   try {
