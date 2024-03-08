faeb2fb2fc976672a823037ad638336bea251048
kimenyu <njorogekimenyu@gmail.com>
2024-03-08 23:19:26 +0300

    changing controllers

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const server = app.listen(process.env.PORT, () =>
 const io = socket(server, {
   cors: {
     origin: "https://chatease-frontend.vercel.app/",
-    credentials: false,
+    credentials: true,
   },
 });
 
